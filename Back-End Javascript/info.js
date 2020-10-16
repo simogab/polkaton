fb17c07e098a17e998607a885c66d08e3160f9f5
simogab <56610676+simogab@users.noreply.github.com>
2020-10-16 17:23:06 +0200

    TODO: structure & bonus

--- Back-End Javascript/info.js
+++ Back-End Javascript/info.js
@@ -1,6 +1,10 @@
+const {ApiPromise, WsProvider} = require('@polkadot/api'); // check required imports
+
+const wsProvider = new WsProvider('wss://rpc.polkadot.io'); //set local node
+
 async function main () {
-    const api = await ApiRx.create().toPromise(); // initialised via static create
-    api.rpc.chain.subscribeNewHeads().subscribe((header) => { 
-      console.log(`Chain is at #${header.number}`);
-});
-}
+    const api = await ApiPromise.create({provider: wsProvider}); // initialise create 
+    const header = await api.rpc.chain.getHeader(); //get latest block info
+    console.log(`Here's all the info: ${header}`); //see full output of latest block info
+};
+main().catch(console.error).finally(() => process.exit());
